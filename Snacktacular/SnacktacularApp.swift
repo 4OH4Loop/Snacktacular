13134d8ceb9d08bc3772f4219f846c5a35299d50
4OH4Loop <semperchuck@gmail.com>
2025-03-30 18:05:24 -0400

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Carolyn Ballinger on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
